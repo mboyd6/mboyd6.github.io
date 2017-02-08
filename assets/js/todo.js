5b8bb5f87279393c84acfd518a5f0a832f4ed76d
Michael Boyd <michaeldavisboyd@gmail.com>
2017-02-07 22:11:10 -0700

    fixed img and icon

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -21,6 +21,6 @@ $("input[type='text']").keypress(function(event){
 
 });
 
-$(".fa-plus").click(function{
-	$("input[type='text'").fadeToggle();
+$(".fa-pencil-square-o").click(function(){
+	$("input[type='text'").fadeToggle()
 });
