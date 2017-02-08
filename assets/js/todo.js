d0943ed2a13c1a3a44ee65b5f0c0fe0bc521d5a7
Michael Boyd <michaeldavisboyd@gmail.com>
2017-02-07 21:43:58 -0700

    added todo list to website

---
+++ assets/js/todo.js
@@ -0,0 +1,26 @@
+// check off specific todos by clicking
+$("ul").on("click", "li", function() {
+// if li is gray
+	$(this).toggleClass("completed");
+});
+//Click on x to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(300, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		// create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText +"</li>");
+		$(this).val("");
+	}
+
+});
+
+$(".fa-plus").click(function{
+	$("input[type='text'").fadeToggle();
+});
